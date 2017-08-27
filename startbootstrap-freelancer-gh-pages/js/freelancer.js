a0ff89d28b09239e47ef51c7318ac1abc36af5b3
jessiealbarian <jessie.albarian@alpin.io>
2017-08-27 12:25:25 -0600

    added pitch deck, made some sass edits

--- startbootstrap-freelancer-gh-pages/js/freelancer.js
+++ startbootstrap-freelancer-gh-pages/js/freelancer.js
@@ -38,9 +38,10 @@
     // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();
     sr.reveal('.sr-title', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
+        duration: 800,
+        scale: 0.5,
+        distance: '0px',
+        delay: 200
     }, 200);
     sr.reveal('.sr-icons', {
         duration: 600,
@@ -51,6 +52,11 @@
         duration: 1000,
         delay: 200
     });
+    sr.reveal('.sr-project', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
     sr.reveal('.sr-contact', {
         duration: 600,
         scale: 0.3,
