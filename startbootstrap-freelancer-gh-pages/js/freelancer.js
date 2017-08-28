11c7b4dad89d863958c7c1f2d18942596189c037
jessiealbarian <jessie.albarian@alpin.io>
2017-08-28 16:04:33 -0600

    added content for popovers and added testimonials section

--- startbootstrap-freelancer-gh-pages/js/freelancer.js
+++ startbootstrap-freelancer-gh-pages/js/freelancer.js
@@ -48,10 +48,10 @@
         scale: 0.3,
         distance: '0px'
     }, 200);
-    sr.reveal('.sr-button', {
-        duration: 1000,
-        delay: 200
-    });
+    // sr.reveal('.sr-button', {
+    //     duration: 1000,
+    //     delay: 200
+    // });
     sr.reveal('.sr-project', {
         duration: 600,
         scale: 0.3,
