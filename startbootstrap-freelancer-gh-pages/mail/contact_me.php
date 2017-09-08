67c08f55cf47db7c6a0585c12c8f436307434228
jessiealbarian <jessie.albarian@alpin.io>
2017-09-07 22:38:25 -0600

    trying to fix contact form

--- startbootstrap-freelancer-gh-pages/mail/contact_me.php
+++ startbootstrap-freelancer-gh-pages/mail/contact_me.php
@@ -16,9 +16,14 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'albarian.jess@gmail.com';
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$email_body = $_POST["You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message"];
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+$headers .= 'From: JessieAlbarian.com' . "\r\n".
+                'Reply-To: ' . $email_address . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
