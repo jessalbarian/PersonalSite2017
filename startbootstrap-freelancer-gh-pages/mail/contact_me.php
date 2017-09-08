f762ef3818ed83d2a4a9c6e960a51c3d5f1314d1
jessiealbarian <jessie.albarian@alpin.io>
2017-09-07 22:50:26 -0600

    trying to fix things

--- startbootstrap-freelancer-gh-pages/mail/contact_me.php
+++ startbootstrap-freelancer-gh-pages/mail/contact_me.php
@@ -1,32 +1,22 @@
 <?php
 if(isset($_POST['submit'])) {
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email']))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-	
-// Create the email and send the message
-$to = 'albarian.jess@gmail.com';
-$email_subject = "Website Contact Form:  $name";
-$email_body = $_POST["You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message"];
+    $email_to = "albarian.jess@gmail.com";
+    $email_subject = "Email from JessieAlbarian.com";
 
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-$headers .= 'From: JessieAlbarian.com' . "\r\n".
-                'Reply-To: ' . $email_address . "\r\n" .
+    $email = $_POST['email'];
+    $yourname =$_POST['name'];
+    $message = $_POST['message'];
+
+        $email_message = $yourname . '<br/>';
+        $email_message .= $message;
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $headers .= 'From: JessieAlbarian.com' . "\r\n".
+                'Reply-To: ' . $email . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
-
-mail($to,$email_subject,$email_body,$headers);
+    mail($email_to, $email_subject, $email_message, $headers);
 } else {
-    echo "Error has occurred with the contact form.";
+    echo "Error occurred";
 }
 ?>
